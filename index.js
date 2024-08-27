1f89a80f360cd939fc40b5bf797e6a54faab2ad3
tegacodess <tegazion7@gmail.com>
2024-08-27 17:11:39 +0100

    chore: Updated js functionality for menus and icon s

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
-// Menu bar functionlity
-document.addEventListener("DOMContentLoaded", function () {
-    const hamburgerMenu = document.querySelector(".hamburger-menu");
-    const navigation = document.querySelector(".navigation");
-    const headerLogo = document.querySelector(".brand-logo");
-    const navLinks = document.querySelectorAll(".navigation a");
+// Services Dropdown Menu Functionality
+document.addEventListener('DOMContentLoaded', function() {
+  const dropbtn = document.querySelector('.dropbtn');
+  const dropdownContent = document.querySelector('.dropdown-content');
+  const dropdownMainIcon = document.querySelector('.dropdown-down');
+
+  dropbtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    dropdownContent.classList.toggle('show');
+    dropdownMainIcon.classList.toggle('rotate');
+  });
+
+  // Close the dropdown if the user clicks outside of it
+  window.addEventListener('click', function(event) {
+    if (!event.target.closest('.dropdown')) {
+      if (dropdownContent.classList.contains('show')) {
+        dropdownContent.classList.remove('show');
+        dropdownMainIcon.classList.remove('rotate')
+      }
+    }
+  });
+
   
-    hamburgerMenu?.addEventListener("click", function () {
-      navigation.classList.toggle("show");
-      headerLogo.classList.toggle("hide");
-    });
+// Hamburger Menu functionlity
+  const hamburgerMenu = document.querySelector(".hamburger-menu");
+  const navigation = document.querySelector(".navigation");
+  const headerLogo = document.querySelector(".brand-logo");
+  const navLinks = document.querySelectorAll(".navigation a");
   
-    navLinks.forEach((link) => {
-      link.addEventListener("click", function () {
-        navigation.classList.remove("show");
-        headerLogo.classList.remove("hide");
-      });
+  hamburgerMenu?.addEventListener("click", function () {
+    navigation.classList.toggle("show");
+    headerLogo.classList.toggle("hide");
+  });
+  
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      navigation.classList.remove("show");
+      headerLogo.classList.remove("hide");
     });
   });
+});
+
+
 
   // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
